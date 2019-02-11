3057a3db215ede6bc78b5490d0bcb57ba2f83997
lucitajulio <lucitajulio@hotmail.com>
2019-02-11 09:13:28 -0600

    subiendo proyecto cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,37 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+
+  //id Mensaje = mensaje.toUpperCase();
+  encode: (edad, Mensaje) => {
+
+    let resultado = " ";//se pone vacio ya que va guardando los resultados
+     
+    for (let i = 0; i < Mensaje.length; i++) {//recorre mensaje
+      let ascii = Mensaje.charCodeAt(i);//aplica cod ascii
+
+      if (ascii >= 65 && ascii <= 90) { //aqui va parametro de cod ascii 65 letra a,90 letra z
+       let mensajeAcodificado = ascii - 65 + parseInt(edad) % 26 + 65;//formula michell
+        resultado += String.fromCharCode(mensajeAcodificado);//me da la letra del cod ascii
+      }
+    } return resultado;
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
+  decode: (edad, Mensaje) => {
+
+    let resultado = " ";//se pone vacio ya que va guardando los resultados
+     
+    for (let i = 0; i < Mensaje.length; i++) {//recorre mensaje
+      let ascii = Mensaje.charCodeAt(i);//aplica cod ascii
+
+      if (ascii >= 65 && ascii <= 90) { //aqui va parametro de cod ascii 65 letra a,90 letra z
+       let mensajeAcodificado = ascii - 90 - parseInt(edad) % 26 + 90 ;//formula michell de atras hacia adelante
+        resultado += String.fromCharCode(mensajeAcodificado);//me da la letra del cod ascii
+      }
+    } return resultado;
+  },
+    //decode: (mensajeParaOffsetedad, mensajeParaCifarMensaje) => { 
+     //for(let i = 0;i<mensajeParaCifar.length; i++);
+     //let mensaje_decodificado = mensajeParaCifar.fromCharCode(i);
+     //resultado += String.charCodeAt(mensaje_decodificado); 
+    //}
+    //return resultado;
+  
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-/* Acá va tu código */
+/*const contenedor = documento.getElementById ("contenedor de datos");
+const  button_enter  =  documento . getElementById ( ' btnIngresar ' );
+button_enter . addEventListener ( ' click ' , ( evento ) => {
+dejar nombre =  documento . getElementById ( ' nombre ' ). valor ;
+documento . getElementById ( ' nombre ' ). valor  =  " " ;
+const  container_datos  =  documento . createElement ( ' div ' );
+container_datos . classList . añadir ( ' contenedor ' );
+contenedor appendChild (container_datos);.*/
+//esto es solo para cifar
+
+
+document.getElementById('cifrar').addEventListener('click', () =>{
+    let Mensaje = document.getElementById('mensaje').value;//toma de la pagina el texto ingresado por el usuario
+    Mensaje = Mensaje.toUpperCase();//pasa el mensaje a mayuscula independiente de como lo escriba el usuario
+    let edad = document.getElementById('edad').value;//value rescata los datos de mi imput de html
+    let resultado = window.cipher.encode(edad, Mensaje);// este toma la funcion para codificar
+    document.getElementById('cajaResultado').innerHTML = resultado;//muestra resultado en la cajaResultado
+
+})
+// aqui solo va a descifrar
+document.getElementById('descifar').addEventListener('click', () =>{
+    let mensajeParaCifar = document.getElementById('mensaje').value;
+    let mensajeParaOffsetedad = document.getElementById('edad').value;//value rescata los datos de mi imput de html
+    let resultado = window.cipher.decode(mensajeParaOffsetedad, mensajeParaCifar);// este toma la funcion para codificar
+    document.getElementById('cajaResultado').innerHTML = resultado;//muestra resultado en la cajaResultado
+
+})
